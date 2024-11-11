a22855cc50a8259b3f71530d4a5e9fea545b8e4d
MonicaKissimina <Monica.kissimina@bellevuecollege.edu>
2024-11-10 17:48:41 -0800

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+<script>
+  let uname = prompt('To continue, please enter your uname: ');
+  let amessage = `You have entered: ${uname}`;
+      console.log(amessage + ' this is an example of a hardcoded text string concatenated after a variable.'); 
+</script>
